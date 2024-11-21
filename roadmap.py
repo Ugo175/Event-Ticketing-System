c029e94f89a117cf65d03fe60cce51715e041905
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-20 23:31:50 -0600

    separated this part to another file

---
+++ roadmap.py
@@ -0,0 +1,21 @@
+print("""
+Register
+Login
+
+Register
+    generate Id, log off
+    if ticket for type_ticket:
+        sugguest other
+            if not want other
+            sorry. bye.
+    Give ticket
+    If NO MORE TICKET:
+        process all.
+
+Login.
+    Cancel
+    downgrade/upgrade if this else downgrde
+
+    if vip full - can't do
+
+""")
