b3e9459f8de7eaf02040aad72974ad2a83df1355
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-20 23:10:00 -0600

    added changes for user options of cancelling summarizing and changing ticket types. Will add conditionals soon.

--- main.py
+++ main.py
@@ -43,16 +43,81 @@ Welcome to our Event Ticketing System. Enter a number to begin.
 1. Register for ticket
 2. Login to make changes
 """)
-        user_action = valid_input(">>> ", {'1', '2'})
-        match user_action:
+        ticket_action = valid_input(">>> ", {'1', '2'})
+        match ticket_action:
+            case '2':
+                user = User.login()
+                if user is None:
+                    print("No user_id with that account exists! Consider creating an account")
+                    break
+                print(f"""
+Hey {user.name}!
+What changes do you want to make to your ticket registration?
+1. Cancel
+2. {user.change}
+3. Summary
+4. Log out
+""")
+                change_action = valid_input(">>> ", )
+                match change_action:
+                    case '1':
+                        user.cancel()
+                    case '2':
+                        user.make_change()
+                    case '3':
+                        user.summarize()
+                    case '4':
+                        print("Thank you for your time!")
             case '1':
                 User.register(ticket_system)
-            case '2':
-                pass
 
     # process tickets
     while ticket_system.tickets:
         print(heapq.heappop(ticket_system))
+
+
+class TicketSystem:
+    """The ticket management class - will have only one instance"""
+    N_VIP = 3
+    N_REGULAR = 7
+    N_TOTAL = N_VIP+N_REGULAR
+    VIP = "VIP"
+    REGULAR = "REGULAR"
+    REG_ACTIONS = {'1', '2'}
+    TICKET_TYPES = {VIP, REGULAR}
+
+    def __init__(self):
+        self.n_vip = 0
+        self.n_regular = 0
+        self.tickets = []  # The starting priority queue
+        self.new_priority = 0
+        # Stop this from having more calls
+        TicketSystem.__init__ = lambda *args: None
+
+    def add_ticket(self, user):
+        """Function to add a ticket/user to the heap"""
+        ticket_type_attr = "n_"+user.ticket.lower()
+        n_ticket_type = getattr(self, ticket_type_attr)
+        setattr(self, ticket_type_attr, n_ticket_type+1)
+        heapq.heappush(self.tickets, (user.priority, user.user_id))
+
+    def remove_ticket(self, ticket_type: str):
+        """Function to subtract from ticket count"""
+        ticket_type_attr = "n_"+ticket_type.lower()
+        n_ticket_type = getattr(self, ticket_type_attr)
+        setattr(self, ticket_type_attr, n_ticket_type-1)
+
+    def process_tickets(self):
+        """Process from the priority queue from first to last
+        Preferring VIPs first
+        """
+        return 3
+
+    @property
+    def n_tickets(self):
+        """return the number of tickets
+        """
+        return self.n_regular+self.n_vip
 
 
 class User:
@@ -72,12 +137,51 @@ class User:
         self.user_id = User.make_id()
         User.users[self.user_id] = self
 
+    def cancel(self, ticket_system):
+        """Remove user from list of ticket registered users"""
+        del User.users[self.user_id]  # Instead of setting 'active'
+        ticket_system.remove_ticket(self.ticket)
+        print("Your registration has been canceled successfully")
+
+    def make_change(self):
+        """Upgrade/Downgrade ticket"""
+        self.ticket = TicketSystem.REGULAR \
+            if self.ticket == TicketSystem.VIP else TicketSystem.VIP
+        print(f"You have successfully changed your ticket type to {self.ticket}")
+
+    def summarize(self):
+        """Provide summary for this user"""
+        print(f"""
+Hello {self.name}!
+You have registered for a {self.ticket} ticket.
+{'How do you feel? Being part of the big folks at the event'\
+  if self.ticket == TicketSystem.VIP
+ else 'It\'s great that you chose to attend this event!'}
+
+""")
+
+    @property
+    def change(self):
+        """The type of change that can be made for this user"""
+        change_prefix = "Down" if self.ticket == TicketSystem.VIP else "Up"
+        new_ticket_type = TicketSystem.REGULAR \
+            if self.ticket == TicketSystem.VIP else TicketSystem.VIP
+        return f"{change_prefix}grade to {new_ticket_type}"
+
+    @staticmethod
+    def login() -> set | None:
+        """Log user in"""
+        user_id = valid_input("Enter your user id: ")
+        if user_id in User.users and User.users[user_id].active:
+            return User.users[user_id]
+        return None
+
     @staticmethod
     def register(ticket_system):
         """Class method to add a new user"""
         name = valid_input("Enter your name: ")
         ticket_type = valid_input(
-            "Enter ticket type: ", valid=TicketSystem.TICKET_TYPES, upper=True)
+            "Enter ticket type ('VIP'/'REGULAR'): ", valid=TicketSystem.TICKET_TYPES, upper=True)
 
         # Check if user can still register
         ticket_type_attr = "n_"+ticket_type.lower()
@@ -96,7 +200,6 @@ Would you like to get a {other_type} ticket instead?
                 ticket_type = other_type
             else:
                 print("Thank you for your time! Good bye!")
-                return
 
         # Instead of the new_priority to be the number of users
         # Make a new class variable to keep track
@@ -104,8 +207,10 @@ Would you like to get a {other_type} ticket instead?
         user = User(name, ticket_type, ticket_system.new_priority)
         ticket_system.new_priority += 1
         ticket_system.add_ticket(user)
-
-        # generateid
+        print(f"""
+Hey {user.name}! Your {user.ticket} ticket request is being processed.
+Your user id is '{user.user_id}'. You can use it to login and change your details.
+""")
 
 
     @staticmethod
@@ -117,7 +222,6 @@ Would you like to get a {other_type} ticket instead?
             user_id = "".join(sample(ascii_lowercase, User.id_length))
         return user_id
 
-
 def valid_input(prompt: str="Input: ", valid: set=None, upper: bool=False) -> str:
     """Function to persist until valid input gotten"""
     answer = ""
@@ -127,47 +231,6 @@ def valid_input(prompt: str="Input: ", valid: set=None, upper: bool=False) -> st
             answer = answer.upper()
     return answer
 
-
-class TicketSystem:
-    """The ticket management class - will have only one instance"""
-    N_VIP = 3
-    N_REGULAR = 7
-    N_TOTAL = N_VIP+N_REGULAR
-    VIP = "VIP"
-    REGULAR = "REGULAR"
-    REG_ACTIONS = {'1', '2'}
-    TICKET_TYPES = {VIP, REGULAR}
-
-    def __init__(self):
-        self.n_vip = 0
-        self.n_regular = 0
-        self.tickets = []  # The starting priority queue
-        self.new_priority = 0
-        print("Called Ticket")
-        # Stop this from having more calls
-        TicketSystem.__init__ = lambda *args: None
-
-    def add_ticket(self, user: User):
-        """Function to add a ticket/user to the heap"""
-        ticket_type_attr = "n_"+user.ticket.lower()
-        n_ticket_type = getattr(self, ticket_type_attr)
-        setattr(self, ticket_type_attr, n_ticket_type+1)
-        heapq.heappush(self.tickets, (user.priority, user.user_id))
-
-
-    def process_tickets(self):
-        """Process from the priority queue from first to last
-        Preferring VIPs first
-        """
-        return 3
-
-    @property
-    def n_tickets(self):
-        """return the number of tickets
-        """
-        return len(self.tickets)
-
-    NAB = 3
 
 class LoggingSystem:
     """Class to log ticket events"""
